14a15b416f97d89e89a87421e98030c4ce4a572e
vikashkumarsingh21 <vk0102103@gmail.com>
2025-12-27 15:46:12 +0530

    fix render deployment

--- app.py
+++ app.py
@@ -252,4 +252,14 @@ def admin():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
